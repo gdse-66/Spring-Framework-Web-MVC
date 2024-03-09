ce622c0aee6cee9005844fa34b132943d6bc0286
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2024-03-09 11:20:20 +0530

    narrow down requests with consumes

---
+++ mapping-spec/src/main/java/lk/ijse/gdse66/spring/api/ItemController.java
@@ -0,0 +1,38 @@
+package lk.ijse.gdse66.spring.api;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/items")
+public class ItemController {
+
+    /* ----------------- consumes ---------------- */
+    /* specify MIME type that a handler method can consume.
+    * spring check content type header of incoming requests (content type of request body) and
+    * route the request to the appropriate handler method.*/
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String SaveItemJson(){
+        return "SaveItemJson(): I accept only application/Json";
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_XML_VALUE)
+    public String SaveItemXml(){
+        return "SaveItemXml(): I accept only application/xml";
+    }
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String SaveItemMultipartFormData(){
+        return "SaveItemMultipartFormData(): I accept only multipart/form-data";
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String SaveItemUrlEncoded(){
+        return "SaveItemUrlEncoded(): I accept only application/x-www-form-urlencoded";
+    }
+
+}
