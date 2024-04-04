95ac8a6108561f18af5faa4e86e14c133cbd41c2
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2024-04-04 12:39:17 +0530

    create query methods

--- spring-web-project/src/main/java/lk/ijse/gdse66/spring/repository/CustomerRepo.java
+++ spring-web-project/src/main/java/lk/ijse/gdse66/spring/repository/CustomerRepo.java
@@ -3,5 +3,14 @@ package lk.ijse.gdse66.spring.repository;
 import lk.ijse.gdse66.spring.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CustomerRepo extends JpaRepository<Customer,String> {
+
+    List<Customer> findCustomersByName(String name); //query methods
+
+    Customer searchByNameAndAddress(String name, String address);
+
+    int countCustomersByAddressStartsWith(String letters);
+
 }
---
+++ spring-web-project/src/test/java/lk/ijse/gdse66/spring/repository/CustomerRepoTest.java
@@ -0,0 +1,65 @@
+package lk.ijse.gdse66.spring.repository;
+
+import lk.ijse.gdse66.spring.config.WebRootConfig;
+import lk.ijse.gdse66.spring.dto.CustomerDTO;
+import lk.ijse.gdse66.spring.entity.Customer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringJUnitConfig(classes = WebRootConfig.class)
+@Transactional
+class CustomerRepoTest {
+
+    @Autowired
+    CustomerRepo customerRepo;
+
+    void addAllCustomers(){
+        Customer customer1 = new Customer("C001", "Kasun Sampath", "Galle", "ProfilePic1");
+        Customer customer2 = new Customer("C002", "Saman Kumara", "Matara", "ProfilePic2");
+        Customer customer3 = new Customer("C003", "Amal Perera", "Matale", "ProfilePic3");
+
+        customerRepo.save(customer1);
+        customerRepo.save(customer2);
+        customerRepo.save(customer3);
+    }
+
+    @Test
+    void saveCustomer(){
+        Customer customer = new Customer(
+                "C001", "Kasun Sampath", "Galle", "ProfilePic1");
+        Customer savedCustomer = customerRepo.save(customer);
+        assertNotNull(savedCustomer);
+    }
+
+    @Test
+    void getCustomersByName(){
+        addAllCustomers();
+        List<Customer> customerList = customerRepo.findCustomersByName("Kasun Sampath");
+        for (Customer customer : customerList) {
+            System.out.println(customer);
+        }
+    }
+
+    @Test
+    void getCustomersByNameAndAddress(){
+        addAllCustomers();
+        Customer customer = customerRepo.searchByNameAndAddress(
+                "Kasun Sampath", "Galle");
+        System.out.println(customer);
+    }
+
+    @Test
+    void countCustomersByStartsWith(){
+        addAllCustomers();
+        int count = customerRepo.countCustomersByAddressStartsWith(
+                "Ma");
+        System.out.println(count);
+    }
+
+}
